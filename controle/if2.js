60c3b56e7920db2973ac6e7d4e95bdc7f254ed35
Guilherme Bersi <33261980+devbersi@users.noreply.github.com>
2020-08-26 14:35:12 -0300

    Add files via upload

---
+++ controle/if2.js
@@ -0,0 +1,10 @@
+function test1(num){
+    if(num > 7)
+        console.log(num)
+    
+     console.log('Final')
+
+}
+
+test1(6)
+test1(8)
---
+++ controle/ifElseIf.js
@@ -0,0 +1,22 @@
+Number.prototype.entre = function (inicio, fim){
+    return this >= inicio && this <= fim
+
+}
+
+const imprimirResultado = function (nota){
+    if(nota.entre(9, 10)){
+        console.log('Quadro de Honra')
+    }
+    else if(nota.entre(7, 8.9)){
+        console.log('Aprovado!')
+    }
+    else if(nota.entre(4, 6.9)){
+        console.log('Recuperação')
+    }
+    else if(nota.entre(0, 3.9)){
+        console.log('Reprovado')
+    }
+    else{
+        console.log('Nota inválida')
+    }
+}
